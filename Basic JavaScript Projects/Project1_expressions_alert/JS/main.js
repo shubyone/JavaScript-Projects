d8af40682d8c5d9d7449d6f0582dc23fa94aa617
Abdul Malik <amalik@outlook.com>
2024-06-05 09:00:20 +0100

    completed assiagnement

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+//Creating two variables
+var Sent1 = "This is the beginnnig of the string "; 
+var Sent2 = "and this is the end of the string.";
+
+document.write("Hello World"); //Using the document.write method
+document.write(Sent1 + Sent2); // Using the document.write method to display variables
+window.alert("This is a JavaScript window alert!!!"); // Window alert method
+
+//This is an expression
+
+document.write(3 + 3);
